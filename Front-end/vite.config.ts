2ae409e437f8b77200261ddbcc4b73b8b8faa5cf
hoapooh <hoapooh2912@gmail.com>
2024-11-06 23:48:01 +0700

    update fix

--- Front-end/vite.config.ts
+++ Front-end/vite.config.ts
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-	base: "/",
 	plugins: [react()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
+		extensions: [".ts", ".json", ".tsx", ".js"],
 	},
+	base: "./",
 })
